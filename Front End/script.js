1449f7d4c1ee1386fc8b0c5da217eaa78e03c4ec
ambrose-vellequette <ambrose@vellequette.com>
2024-02-17 13:47:57 -0800

    Update script.js

--- Front End/script.js
+++ Front End/script.js
@@ -14,3 +14,29 @@ document.getElementById('userInfoForm').addEventListener('submit', function(even
     // Now you can use these values to make API calls, update the UI, etc.
     console.log(name, location, radius, interests);
 });
+
+function sendBackend() {
+    var userInput;
+    userInput = "liquor stores near me";
+    
+    url = "" + userInput;
+    fetch(url, {
+        method: 'GET',
+    }).then(response => {
+        //see if response is OK
+        if(!response.ok) {
+            throw new Error('HTTP error! Status: ${response.status}');
+        }
+            return response.json();
+    })
+    .then(data =>{
+        //do stuff to data
+        console.log(data);
+    })
+    .catch(error => {
+        //deal with errors
+        console.error('Error:', error);
+    });
+
+
+}
