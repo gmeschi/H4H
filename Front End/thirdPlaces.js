9df1baf7efa092a76a7f4a15dbec57837fa887ea
Daniel Louie <Daniel.louie27@gmail.com>
2024-02-17 14:21:57 -0800

    Update thirdPlaces.js

--- Front End/thirdPlaces.js
+++ Front End/thirdPlaces.js
@@ -2,11 +2,26 @@
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+// Get the form element
+var form = document.getElementById('searchForm');
+
+// Add an event listener for the submit event
+form.addEventListener('submit', function(event) {
+    // Prevent the form from being submitted normally
+    event.preventDefault();
+
+    // Get the input values
+    var location = document.getElementById('location').value;
+    var activity = document.getElementById('activity').value;
+    var category = document.getElementById('category').value;
+
+    // Pass the input values to the sendBackend function
+    sendBackend(location, activity, category);
+});
 
 function sendBackend(location, activity, category) {
-    var userInput;
+    var userInput = location + " " + activity + " " + category;
     userInput = "liquor stores near me";
-    userInput = ""
     //what should this url be??
     //"http://127.0.0.1:5500/getMain?=" + input
     url = "http://127.0.0.1:5500/Front%20End/thirdPlaces.html?input=" + userInput;
