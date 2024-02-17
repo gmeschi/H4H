61b5c6ccd3e2dc5c2aa2ea604932a6199f73d879
Daniel Louie <Daniel.louie27@gmail.com>
2024-02-17 12:07:12 -0800

    Added User Info Collection

--- Front End/script.js
+++ Front End/script.js
@@ -17,3 +17,16 @@ function openTab(evt, tabName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+document.getElementById('userInfoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const location = document.getElementById('location').value;
+    const radius = document.getElementById('radius').value;
+    const interests = Array.from(document.getElementById('interests').selectedOptions).map(option => option.value);
+
+    // Now you can use these values to make API calls, update the UI, etc.
+    console.log(name, location, radius, interests);
+});
