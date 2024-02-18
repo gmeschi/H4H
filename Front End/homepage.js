5c0de33d1e3863a3859ea2325bbbe41c5bf7e0bd
jbamboozleDD <jackdseattle@outlook.com>
2024-02-17 19:09:26 -0800

    map on homepage

---
+++ Front End/homepage.js
@@ -0,0 +1,13 @@
+/*function initMap() {
+    var location = {lat: -34.397, lng: 150.644}; // Replace with your location
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: location
+    });
+
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}*/
